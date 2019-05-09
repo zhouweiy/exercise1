d66c297e95e0e5c5ff914207e27148c6f599a201
zhouweiy <50460369+zhouweiy@users.noreply.github.com>
2019-05-09 13:44:24 -0400

    Update hello_world.php

--- hello_world.php
+++ hello_world.php
@@ -7,19 +7,6 @@
 <body>
 
 <?php
-/*
-  Author: Your Instructor
-  File name: hello_world.php
-  Date created: Summer 2018
-  Description: 
-	example of hybrid PHP to demonstrate how and where php code can be placed 
-	inside HTML code
-	all code outside the start and stop processing instructions is ignored
-	by the PHP intepreter
-	all code inside will be processed by the PHP interpreter and replaced
-	by the results of the processing
-*/
-
 // declare and initialize variable $txt to store the value Hello World
 $txt = "Hello World!";
 
